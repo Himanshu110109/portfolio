916440e94efe030eb59f74bd7aa14033bdeccc12
Sanskari coder <158292948+Himanshu110109@users.noreply.github.com>
2026-05-23 07:36:22 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -97,7 +97,7 @@ export default function Projects() {
         </p>
 
         <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold gradient-text">
-          Projects
+          Products
         </h2>
       </div>
 
